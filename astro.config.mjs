3fc4ba4126d9322cfdd170c6f368cfafa0cc7b1d
Lawhornmatt <66797498+Lawhornmatt@users.noreply.github.com>
2023-04-27 01:52:58 -0500

    Custom Domain SetUp

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({  
-    site: 'https://lawhornmatt.github.io',
-    base: '/AstroPortfolio_Dev',
+    site: 'lawhornmatt.com',
+    // site: 'https://lawhornmatt.github.io',
+    // base: '/AstroPortfolio_Dev',
 });
